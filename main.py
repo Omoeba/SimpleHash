4677885031665545f5cbbfd2d7242df36ad3362a
Omoeba <38597972+omoeba@users.noreply.github.com>
2019-10-13 22:50:34 -0700

    bug fixes and new features

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import hashlib
-import pyperclip
-import shutil
 import getpass
+import hashlib
+import shutil
+
+import pyperclip
+
+pass_string = None
 
 def ts():
     return shutil.get_terminal_size()
@@ -11,20 +14,38 @@ def lineSeparator():
     return "━" * ts().columns
 
 
-def hexPrinter(hexValue):
+def printSplitter(inputString):
     i = 1
-    endIndex = len(hexValue)
+    endIndex = inputString.rindex(" ", 0, len(inputString))
+    while True:
+        lineIndex = inputString.rindex(" ", 0, (ts().columns * i))
+        inputString = list(inputString)
+        if lineIndex == endIndex:
+            break
+        else:
+            inputString[lineIndex] = '\n'
+            inputString = "".join(inputString)
+        i = i + 1
+    inputString = list(inputString)
+    inputString.pop()
+    inputString = "".join(inputString)
+    return inputString
+
+
+def hashPrinter(hashValue):
+    i = 1
+    endIndex = len(hashValue)
     while True:
         lineIndex = ts().columns * i
-        hexValue = list(hexValue)
+        hashValue = list(hashValue)
         if lineIndex >= endIndex:
             break
         else:
-            hexValue[lineIndex] = '\n'
-            hexValue = "".join(hexValue)
+            hashValue.insert(lineIndex, '\n')
+            hashValue = "".join(hashValue)
         i = i + 1
-    hexValue="".join(hexValue)
-    return hexValue
+    hashValue = "".join(hashValue)
+    return hashValue
 
 
 def hasher(target):
@@ -32,10 +53,68 @@ def hasher(target):
         hashlib.sha256(target.encode()).hexdigest()
     return sha_signature
 
-while True:
+
+def hashPass():
+    global pass_string
     print(lineSeparator())
-    hash_string = getpass.getpass()
-    sha_signature = hasher(hash_string)
+    pass_string = getpass.getpass()
+    sha_signature = hasher(pass_string)
     pyperclip.copy(sha_signature)
     print(lineSeparator())
-    print(hexPrinter(sha_signature))
+    print(hashPrinter(sha_signature))
+
+
+def newPass():
+    global pass_string
+    while True:
+        print(lineSeparator())
+        pass_string = getpass.getpass()
+        checkPass_string = getpass.getpass('Confirm Password: ')
+        if checkPass_string != pass_string:
+            print(lineSeparator())
+            print(printSplitter("Password and confirmation do not match. "))
+            continue
+        sha_signature = hasher(pass_string)
+        pyperclip.copy(sha_signature)
+        print(lineSeparator())
+        print(hashPrinter(sha_signature))
+        break
+
+
+def main():
+    global pass_string
+    while True:
+        print(lineSeparator())
+        mode = str(input(printSplitter(
+            "Type \"1\" to get the hash of your password, \"2\" to create a new (hashed) password, \"plaintext\" to get the previous inputted password in plaintext form, \"clear\" to clear the program window and previously cached passwords, \"refresh\" to re-display the prompt (useful if you change the size of the program window), and \"quit\" to exit.\n:  ")))
+        if mode == "1":
+            hashPass()
+        elif mode == "2":
+            newPass()
+        elif mode == "quit":
+            print(printSplitter(" " * (ts().columns * ts().lines)))
+            break
+        elif mode == "plaintext":
+            if pass_string != None:
+                print(lineSeparator())
+                print(pass_string)
+            else:
+                print(lineSeparator())
+                print(printSplitter("No previous inputs. "))
+                continue
+        elif mode == "clear":
+            try:
+                pass_string = None
+            except:
+                pass
+            print(printSplitter(" " * (ts().columns * ts().lines)))
+            continue
+        elif mode == "refresh":
+            continue
+        else:
+            print(lineSeparator())
+            print(printSplitter("Invalid option. "))
+            continue
+
+
+main()
