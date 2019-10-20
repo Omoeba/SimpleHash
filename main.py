6067432d4ef22e2856eb9cf83ef2aaf5e35f5366
Omoeba <38597972+Omoeba@users.noreply.github.com>
2019-10-19 19:00:41 -0700

    Update to argon2 and new features.

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
+import base64
 import getpass
 import hashlib
+import os
+import re
 import shutil
 
+import argon2
+import numpy as np
 import pyperclip
 
 pass_string = None
+savelist = None
+
+savefile = os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), 'savefile.npy')
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def ts():
     return shutil.get_terminal_size()
+
+
+def cleanInput(inputString, mode="string"):
+    if mode == "string":
+        return re.sub(r'[^a-zA-Z0-9]', '', inputString)
+    elif mode == "email":
+        return re.sub(r'[^a-zA-Z0-9@.]', '', inputString)
+    elif mode == "name":
+        return re.sub(r'[^a-zA-Z]', '', inputString)
 
 
 def lineSeparator():
     return "━" * ts().columns
 
 
-def printSplitter(inputString):
-    i = 1
-    endIndex = inputString.rindex(" ", 0, len(inputString))
-    while True:
-        lineIndex = inputString.rindex(" ", 0, (ts().columns * i))
-        inputString = list(inputString)
-        if lineIndex == endIndex:
-            break
-        else:
-            inputString[lineIndex] = '\n'
-            inputString = "".join(inputString)
-        i = i + 1
-    inputString = list(inputString)
-    inputString.pop()
-    inputString = "".join(inputString)
-    return inputString
+def stringSplitter(inputString):
+    outputString = ""
+    while len(inputString) >= ts().columns:
+        breakIndex = inputString.rindex(" ", 0, ts().columns)
+        newString = inputString[:breakIndex] + '\n'
+        inputString = inputString[breakIndex + 1:]
+        outputString = outputString + newString
+    outputString = outputString + inputString
+    return outputString
 
 
 def hashPrinter(hashValue):
@@ -48,73 +63,158 @@ def hashPrinter(hashValue):
     return hashValue
 
 
-def hasher(target):
-    sha_signature = \
-        hashlib.sha256(target.encode()).hexdigest()
-    return sha_signature
+def hasher(target, keyName, fullName, email):
+    try:
+        salt_len = 32 + len(target.decode())
+        salt = hashlib.shake_256(keyName + fullName + email).digest(salt_len)
+        sha_signature = base64.b64encode(
+            argon2.low_level.hash_secret_raw(target, salt, argon2.DEFAULT_TIME_COST, argon2.DEFAULT_MEMORY_COST,
+                                             argon2.DEFAULT_PARALLELISM, argon2.DEFAULT_HASH_LENGTH,
+                                             argon2.low_level.Type.ID)).decode()
+        return sha_signature
+    except:
+        raise ValueError
 
 
 def hashPass():
     global pass_string
-    print(lineSeparator())
-    pass_string = getpass.getpass()
-    sha_signature = hasher(pass_string)
-    pyperclip.copy(sha_signature)
-    print(lineSeparator())
-    print(hashPrinter(sha_signature))
+    while True:
+        print(lineSeparator())
+        print(stringSplitter("Type \"quit\" to exit."))
+        print(lineSeparator())
+        keyName = cleanInput(input("Key Name\n: ").lower())
+        print(keyName)
+        if keyName == "quit":
+            break
+        try:
+            fullName = (base64.b64decode((np.load(savefile))[0])).decode()
+            print(lineSeparator())
+            print(stringSplitter("Name loaded from save file."))
+        except:
+            fullName = cleanInput(input("Your Name\n: ").lower(), "name")
+            print(fullName)
+            if fullName == "quit":
+                break
+        try:
+            email = (base64.b64decode((np.load(savefile))[1])).decode()
+            print(lineSeparator())
+            print(stringSplitter("Email loaded from save file."))
+            print(lineSeparator())
+        except:
+            email = cleanInput(input("Email\n: ").lower(), "email")
+            print(email)
+            if email == "quit":
+                break
+        pass_string = getpass.getpass()
+        try:
+            sha_signature = hasher(pass_string.encode(), keyName.encode(), fullName.encode(), email.encode())
+            pyperclip.copy(sha_signature)
+            print(lineSeparator())
+            print(hashPrinter(sha_signature))
+        except ValueError:
+            print(lineSeparator())
+            print(stringSplitter("Invalid Password."))
+        break
 
 
 def newPass():
     global pass_string
+    global savelist
+    helpInfo = stringSplitter("Password is case sensitive.") + '\n\n' + stringSplitter(
+        "Key Name should be something easily recognizable, such as the name of the website or service you are "
+        "using the generated password for; e.g. \"apple\" or \"gpg\". Key Name is case insensitive.") + '\n\n' + stringSplitter(
+        "Name and email are optional and are an easy to remember way to make the resulting hash more random, "
+        "thus more secure. No parts of your name or email will be obtainable by attackers attempting to crack the "
+        "hash. Type \"save\" after creating a password to save your name and email to disk so you don't have to enter "
+        "it again when getting the hash again. Name and email are case insensitive.") + '\n\n' + stringSplitter(
+        "Type \"quit\" to exit and \"help\" to re-display the prompt.")
     while True:
         print(lineSeparator())
+        print(helpInfo)
+        print(lineSeparator())
+        keyName = cleanInput(input("Key Name\n: ").lower())
+        if keyName == "quit":
+            break
+        elif keyName == "help":
+            continue
+        fullName = cleanInput(input("Your Name\n: ").lower(), "name")
+        if fullName == "quit":
+            break
+        elif fullName == "help":
+            continue
+        email = cleanInput(input("Email\n: ").lower(), "email")
+        if email == "quit":
+            break
+        elif email == "help":
+            continue
         pass_string = getpass.getpass()
         checkPass_string = getpass.getpass('Confirm Password: ')
         if checkPass_string != pass_string:
             print(lineSeparator())
-            print(printSplitter("Password and confirmation do not match. "))
-            continue
-        sha_signature = hasher(pass_string)
-        pyperclip.copy(sha_signature)
-        print(lineSeparator())
-        print(hashPrinter(sha_signature))
+            print(stringSplitter("Password and confirmation do not match."))
+            break
+        try:
+            sha_signature = hasher(pass_string.encode(), keyName.encode(), fullName.encode(), email.encode())
+            savelist = [base64.b64encode(fullName.encode()).decode(), base64.b64encode(email.encode()).decode()]
+            pyperclip.copy(sha_signature)
+            print(lineSeparator())
+            print(hashPrinter(sha_signature))
+        except ValueError:
+            print(lineSeparator())
+            print(stringSplitter("Invalid Password"))
         break
 
 
 def main():
     global pass_string
+    global savelist
+    global savefile
     while True:
         print(lineSeparator())
-        mode = str(input(printSplitter(
-            "Type \"1\" to get the hash of your password, \"2\" to create a new (hashed) password, \"plaintext\" to get the previous inputted password in plaintext form, \"clear\" to clear the program window and previously cached passwords, \"refresh\" to re-display the prompt (useful if you change the size of the program window), and \"quit\" to exit.\n:  ")))
+        mode = cleanInput(str(input(stringSplitter(
+            "Type \"1\" to calculate and existing hash, \"2\" to create a new password hash, \"plaintext\" to get the "
+            "previous inputted password in plaintext form, \"clear\" to clear the program window and previously "
+            "cached passwords, \"refresh\" to re-display the prompt (useful if you change the size of the program "
+            "window), \"delete\" to delete any saved name or email, and \"quit\" to exit.\n: "))).lower())
         if mode == "1":
             hashPass()
         elif mode == "2":
             newPass()
         elif mode == "quit":
-            print(printSplitter(" " * (ts().columns * ts().lines)))
+            clear()
             break
         elif mode == "plaintext":
-            if pass_string != None:
+            if pass_string is not None:
                 print(lineSeparator())
-                print(pass_string)
+                print(hashPrinter(pass_string))
             else:
                 print(lineSeparator())
-                print(printSplitter("No previous inputs. "))
-                continue
+                print(stringSplitter("No previous inputs."))
         elif mode == "clear":
-            try:
-                pass_string = None
-            except:
-                pass
-            print(printSplitter(" " * (ts().columns * ts().lines)))
-            continue
+            pass_string = None
+            clear()
         elif mode == "refresh":
-            continue
+            pass
+        elif mode == "save":
+            if savelist is not None:
+                print(lineSeparator())
+                print(stringSplitter("Saving..."))
+                np.save(savefile, savelist)
+                print(stringSplitter("Success!"))
+            else:
+                print(lineSeparator())
+                print(stringSplitter("Nothing to save yet..."))
+        elif mode == "delete":
+            try:
+                print(lineSeparator())
+                print(stringSplitter("Deleting..."))
+                os.remove(savefile)
+                print(stringSplitter("Done!"))
+            except:
+                print(stringSplitter("Nothing to delete."))
         else:
             print(lineSeparator())
-            print(printSplitter("Invalid option. "))
-            continue
+            print(stringSplitter("Invalid option."))
 
 
 main()
